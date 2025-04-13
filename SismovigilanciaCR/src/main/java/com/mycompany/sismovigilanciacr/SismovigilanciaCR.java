7c473e6645944641b2839212af870d8410f8b09a
JosueSantiago25 <josuehidalgos2511@gmail.com>
2025-04-12 21:41:17 -0600

    ProjectCreation
    NetBeans Created

---
+++ SismovigilanciaCR/src/main/java/com/mycompany/sismovigilanciacr/SismovigilanciaCR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sismovigilanciacr;
+
+/**
+ *
+ * @author Usuario1
+ */
+public class SismovigilanciaCR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
