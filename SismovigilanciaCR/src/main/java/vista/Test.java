f678e879568723412c1399fb5dfd16da00cda8d6
Javier184824 <64340244+Javier184824@users.noreply.github.com>
2025-05-11 18:30:46 +0000

    Add files via upload

---
+++ SismovigilanciaCR/src/main/java/vista/Test.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+import modelo.Excel;
+
+/**
+ *
+ * @author javie
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        Excel excel = new Excel("excelTest.xlsx");
+        excel.getListaSismos();
+        System.out.println(excel.getSismos());
+        String[] param = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        excel.agregarSismo(param);
+        System.out.println(excel.getSismos());
+        excel.guardarDatos();
+        //System.out.println(excel.getSismos());
+    }
+}
